c63a5356e2f5aa2b98cb4fc2f4df50184f9c06b6
Martin Ellis <martin.ellis@gov.scot>
2021-11-05 17:43:37 +0000

    Remove grunt files from debian package and delete tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,20 +13,12 @@ module.exports = function(grunt) {
     // copy the tempdir from config weaver to the grunt config.
     gruntConfig.tempdir = require('config-weaver').config().tempdir;
 
-    var config = path.join(process.cwd(), 'grunt/config');
-    var devel = path.join(process.cwd(), 'grunt/devel');
-    var global = path.join(process.cwd(), 'grunt');
-    var configPath = [devel, config, global];
-
-    grunt.loadTasks('tasks');
-
     require('load-grunt-config')(grunt, {
         init: true,
         data: {
             site: gruntConfig,
             tempdir: gruntConfig.tempdir
         },
-        configPath: configPath,
         loadGruntTasks: false
     });
 
